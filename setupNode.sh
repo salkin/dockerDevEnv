a090f7458b5b01d3f4ea766f032a59bb907ada0d
Wik Niklas Peter Jakob <niklas.wik@nsn.com>
2016-10-12 10:14:36 +0300

    Setup node ip fix

--- setupNode.sh
+++ setupNode.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 CONSUL_NODE=$1
-OWN_HOST=$(ip -4 -br address | grep eth1 | awk '{ split({$3,a,"/"); print a[1]}')
+OWN_HOST=$(ip -4 -br address | grep eth1 | awk '{ split($3,a,"/"); print a[1]}')
 
-exist=$(docker ps -a | grep "swarm join" -m 1 | awk '{ print ( }')
+exist=$(docker ps -a | grep "swarm join" -m 1 | awk '{ print $1 }')
 if [[ -z ${exist}  ]];
 then
 docker run -d swarm join --advertise=${OWN_HOST}:2375 consul://${CONSUL_NODE}:8500
