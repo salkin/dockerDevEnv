6dfccb7e809f44ff636e26fb0ffd43868d044e5c
Wik Niklas Peter Jakob <niklas.wik@nsn.com>
2016-10-13 21:22:17 +0300

    Fix path

--- setupMaster.sh
+++ setupMaster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 install_compose() {
    curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-   chmod +x /usr/bin/docker-compose
+   chmod +x /usr/local/bin/docker-compose
 }
 
 OWN_IP=$(ip -4 -br address | grep eth1 | awk '{ split($3,a,"/"); print a[1]'})
